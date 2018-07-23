ba95123b1f96415e89049292a4bf08669d42c084
Matheus Lazarin <lazarin.matheus@gmail.com>
2018-07-23 18:43:10 -0300

    All async functions implemented using Ajax.

--- public/javascripts/mainpage.js
+++ public/javascripts/mainpage.js
@@ -1,67 +1,86 @@
-$(document).ready(function() {
+$(document).ready(function () {
+  //set focus to the input
   $("#tsk").focus();
 
-  $(".span-unchecked").on("click", function () {
-    if($(this).html() === "radio_button_unchecked") {
-      $(this).html("&#xe089;");
-      $(this).next().css({"text-decoration" : "line-through"});
-    } else {
-      $(this).html("radio_button_unchecked");
-      $(this).next().css({"text-decoration" : "none"});
-    }
-    $(this).toggleClass("material-icons span-unchecked glyphicon green");
-  });
-
-  $(".specs-delete").click(function() {
-      return confirm('Are you sure you want to delete?');
-  });
-
-  $("#insert-btn").on("click", function() {
-    if ( !$("#tsk").val() ) {
-      alert("Please type a new task!");
-      $("#tsk").focus();
-      return false;
-    }
-  });
-
-  function showNewItem (response) {    
+  //show new item
+  function showNewItem(response) {
     var prepend = $('.specs-input');
-    // $("body").delegate("span", "click", function() {
-      prepend.before('\
-      <div class="row specs-row">\
+    prepend.before('\
+    <div class="row specs-row" id= "' + response._id + '">\
         <span class="material-icons span-unchecked specs-check">radio_button_unchecked</span>\
         <input type="text" value = "' + response.task + '" class="form-control specs-item" readonly>\
-        <a href="/delete/' +  response._id + '" class="specs-delete"><span class="glyphicon glyphicon-trash "></span></a>\
+        <span class="glyphicon glyphicon-trash specs-delete"></span>\
       </div>');
-    // });
   }
 
-  // function showItems () {
-  //   $.ajax({
-  //     url: '/items',
-  //     method: 'GET',
-  //     contentType: 'application/json',
-  //     success: function(response) {
-        
-  //     }
-  //   })
-  // }
-
+  //create a new item using ajax
   $("#create-form").on("submit", function (event) {
     event.preventDefault();
 
     var myTask = $("#tsk");
-    
+
     $.ajax({
       url: '/items',
       method: 'POST',
       contentType: 'application/json',
       data: JSON.stringify({ "task": myTask.val() }),
-      success: function(response) {
+      success: function (response) {
         showNewItem(response);
         myTask.val('');
       }
     });
+  });
+
+  //update an item using ajax
+  $("body").delegate(".specs-check", "click", function () {
+    
+    var id = $(this).parent().attr('id');
+    console.log(id);
+
+    $.ajax({
+      url: '/items/' + id,
+      method: 'PUT',
+      contentType: 'application/json',
+      success: function(response) {
+          console.log(response);
+      }
+    });
+    
+    if ($(this).html() === "radio_button_unchecked") {
+      $(this).html("&#xe089;");
+      $(this).next().css({ "text-decoration": "line-through" });
+    } else {
+      $(this).html("radio_button_unchecked");
+      $(this).next().css({ "text-decoration": "none" });
+    }
+    $(this).toggleClass("material-icons span-unchecked glyphicon green");
+  });
+
+  //delete an item using ajax
+  $("body").delegate(".specs-delete", "click", function () {
+    if (confirm('Are you sure you want to delete?')) {
+      var id = $(this).parent().attr('id');
+      $.ajax({
+        url: '/items/' + id,
+        method: 'DELETE',
+        contentType: 'application/json',
+        success: function (response) {
+          console.log(response);
+        }
+      });
+      $(this).parent().remove();
+    } else {
+      return false;
+    }
+  });
+
+  //validate input if empty
+  $("#insert-btn").on("click", function () {
+    if (!$("#tsk").val()) {
+      alert("Please type a new task!");
+      $("#tsk").focus();
+      return false;
+    }
   });
 });
 
--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var router = express.Router();
 
-//show index page listing the checklist
+//show index page listing the items
 router.get('/', function(req, res) {
   global.db.findAll((e, items) => {
       if(e) { return console.log(e); }
@@ -9,67 +9,44 @@ router.get('/', function(req, res) {
   });
 });
 
-router.get('/items', function(req, res) {
-  global.db.findAll((e, items) => {
-    if(e) { return console.log(e); }
-    res.send({ items: items });
-  });
-});
-
-
-//show new checklist item page
-router.get('/new', function(req, res, next) {
-  res.render('new', { title: 'New Item', doc: {"task":"","status":""}, action: '/new' });
-});
-
-//add new item to checklist
+//add new item
 router.post('/items', function(req, res) {
   var task = req.body.task;
-  var id = req.body.id;
   var status = 'not done';
   global.db.insert({task, status}, (err, result) => {
           if(err) { return console.log(err); }
           res.send(result.ops[0]);
-      })
-})
-
-
-//show update checklist item page
-// router.get('/edit/:id', function(req, res, next) {
-//   var id = req.params.id;
-//   global.db.findOne(id, (e, docs) => {
-//       if(e) { return console.log(e); }
-//       res.render('new', { title: 'Edição de Cliente', doc: docs[0], action: '/edit/' + docs[0]._id });
-//     });
-// });
-
-//call update function and return to index page
-router.get('/edit/:id', function(req, res) {
-  var id = req.params.id; //maybe it should be body instead of params
-  var status = req.params.status;
-  console.log('this is the status' + status);
-
-  if(status==="done") {
-    console.log('entered if');
-    status = "not done";
-  } else {
-    console.log('entered else');
-    status = "done";
-  }
-
-  console.log(status);
-  global.db.update(id, status, (e, result) => {
-        if(e) { return console.log(e); }
-        res.redirect('/');
-    });
+      });
 });
 
-router.get('/delete/:id', function(req, res) {
+//update item status
+router.put('/items/:id', function(req, res) {
   var id = req.params.id;
+  var myItem = {};
+  var status = "";
+  global.db.findOne(id, (e, items) => {
+    if(e) { return console.log(e); }
+    myItem = { items };
+    status =  myItem.items[0].status;
+    console.log("my status:" + status);
+    if(status==="done") {
+      status = "not done";
+    } else {
+      status = "done";
+    }
+    global.db.update(id, status, (e, result) => {
+            if(e) { return console.log(e); }
+            res.send('success!');
+        });
+  });
+});
+
+//delete item
+router.delete('/items/:id', function(req, res) {
+  var id = req.params.id.toString();
   global.db.deleteOne(id, (e, r) => {
-        if(e) { return console.log(e); }
-        res.redirect('/');
-      });
+    res.send('success!');
+  });
 });
 
 module.exports = router;
